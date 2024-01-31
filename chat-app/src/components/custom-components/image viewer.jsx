018874d71a4eba50530cdc9495d815f5b24b91d8
Pranjal Pravesh <pranjalpravesh222@gmail.com>
2024-01-31 17:23:36 +0530

    updated image viewer and contact info

--- chat-app/src/components/custom-components/image viewer.jsx
+++ chat-app/src/components/custom-components/image viewer.jsx
@@ -3,6 +3,7 @@ import { motion, AnimatePresence } from 'framer-motion';
 
 export default function ImageViewer(props) {
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [scale, setScale] = useState(1);
 
     const handleOpenModal = () => {
         setIsModalOpen(true);
@@ -10,10 +11,27 @@ export default function ImageViewer(props) {
 
     const handleCloseModal = () => {
         setIsModalOpen(false);
+        setScale(1);
     };
 
     const handleModalContentClick = (event) => {
         event.stopPropagation(); // Prevents click events from bubbling up to the overlay
+    };
+
+    
+
+    const handleWheel = (event) => {
+        const delta = event.deltaY;
+
+        if (delta > 0) {
+             // Scrolling down - zoom out
+          if  (scale>0.2){
+            setScale(scale - 0.1)};
+        } else {
+            // Scrolling up - zoom in
+            if  (scale<2.5){
+                setScale(scale + 0.1)};
+        }
     };
 
     return (
@@ -25,7 +43,9 @@ export default function ImageViewer(props) {
                     aspectRatio: '1',
                     cursor: 'pointer',
                     opacity: isModalOpen ? '0.7' : '1',
+                    ...props.style
                 }}
+                whileHover={{opacity:'0.7'}}
                 onClick={handleOpenModal}
             />
 
@@ -47,17 +67,20 @@ export default function ImageViewer(props) {
                             justifyContent: 'center',
                             zIndex: 1000,
                         }}
+                        onWheel={handleWheel}
                         onClick={handleCloseModal}
                     >
                         <motion.div
-                            initial={{ scale: 0 }}
-                            animate={{ scale: 1 }}
+                            // initial={{ scale: 0 }}
+                            animate={{ scale: scale }}
+                            transition={{ type: "linear"}}
                             exit={{ scale: 0 }}
                             style={{
                                 position: 'relative',
                                 maxWidth: '90%',
                                 maxHeight: '90%',
                             }}
+                            
                             onClick={handleModalContentClick} // Stop propagation from modal content
                         >
                             <motion.img
--- chat-app/src/components/main-screen/chat-tab/contact-detail.jsx
+++ chat-app/src/components/main-screen/chat-tab/contact-detail.jsx
@@ -1,10 +1,14 @@
-import { Avatar, Button, IconButton, Stack, Switch, Typography, styled } from "@mui/material";
+import { Avatar, Button, IconButton, Stack, Switch, Tab, Tabs, ThemeProvider, Typography, createTheme, styled } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleContactDetail } from "../../../features/contact-detail";
-import {  Bell, CaretRight, Phone, Star, VideoCamera, X } from "@phosphor-icons/react";
+import {  Bell, CaretLeft, CaretRight, Phone, Star, VideoCamera, X } from "@phosphor-icons/react";
 import ImageViewer from "../../custom-components/image viewer";
+import { useState } from "react";
 
 export default function ContactDetail(){
+
+    const [currentWindow, setCurrentWindow] = useState('basicDetail')
+    
     const dispatch =  useDispatch();
     const theme = useSelector(state=> state.theme);
     var isVisible = useSelector(state => state.contactDetail);
@@ -15,84 +19,191 @@ export default function ContactDetail(){
         }}));
 
 
-    return (
+    function BasicDetail(){
+
+        return (
         
-        <div style={{ display:isVisible?"flex":'none',
-                    flexDirection:'column',
-                    width:'360px', 
-                    height:'100svh', 
-                    background: theme==="light"?'rgb(249 249 249)':"black",
-                    transition:'background width 200ms', 
-                    padding:'15px 20px',   
-                    overflow:'auto',
-                    flexShrink:'0',
-                    }}
-                    className="customScrollbar">
+            <div style={{ display:isVisible?"flex":'none',
+                        flexDirection:'column',
+                        width:'360px', 
+                        height:'100svh', 
+                        background: theme==="light"?'rgb(249 249 249)':"black",
+                        transition:'background width 200ms', 
+                        padding:'15px 20px',   
+                        overflow:'auto',
+                        flexShrink:'0',
+                        }}
+                        className="customScrollbar">
+                    
                 
+                
+                    <IconButton onClick={()=>dispatch(toggleContactDetail())} 
+                        sx={{color:theme==='dark'?'white':'black', position:'absolute', top:'7px', right:'7px'}}>
+                        <X size={20} weight="light" />
+                    </IconButton>
+
+                
+
+                <Stack sx={{alignItems:'center',}}>
+                    <Avatar sx={{width:'150px', height:'150px'}}
+                            src='https://media.licdn.com/dms/image/D4E03AQHBqWeBZgzLOQ/profile-displayphoto-shrink_800_800/0/1682102966918?e=2147483647&v=beta&t=3h218jhABSX_wWq-yVt8-yCUjO12cSvq-srN_tkYPg8'/>
+                
+                    <Typography variant="h6" sx={{ color:theme==='dark'?'white':'black', marginTop:'2px'}}>Pranjal Pravesh</Typography>
+                    <Typography variant="h7" sx={{ color:'grey'}}>@pranjal.pravesh</Typography>
+                </Stack>
+
+
+                <Stack direction={'row'} sx={{justifyContent:'center', marginTop:'10px', gap:'30px'}}>
+                    <IconButton sx={{color:theme==='dark'?'white':'black'}}><Phone size={40} weight="thin" /></IconButton>
+                    <IconButton sx={{color:theme==='dark'?'white':'black'}}><VideoCamera size={40} weight="thin" /></IconButton>
+                </Stack>
+
+
+                    <Typography sx={{ color:theme==='dark'?'white':'black', margin:'20px 0 0 0', fontSize:'17px'}}>About</Typography>
+                    <Typography sx={{ color:'grey', lineHeight:'18px'}}>
+                        My wave function collapses when you see me.
+                    </Typography>
+                
+
+                
+                <Stack sx={{justifyContent:'space-between', marginTop:'30px', alignItems:'center', cursor:'pointer'}} direction={'row'}
+                        onClick={()=>{setCurrentWindow('')}}>
+                    <Typography sx={{ color:theme==='dark'?'white':'black', fontSize:'17px'}}>Media, Links & Docs</Typography>
+                    <CaretRight size={20} style={{color:theme==='dark'?'white':'black'}}/>
+                </Stack>
+
+                <Stack direction={'row'} sx={{gap:'5px', justifyContent:'center', paddingTop:'15px'}}>
+
+                    <ImageViewer src='https://strapi.dhiwise.com/uploads/618fa90c201104b94458e1fb_64fecc21e15ea565176c74ad_React_Framer_Motion_Main_Image_07ee7c735e.jpg'/>
+                    <ImageViewer src='https://picsum.photos/id/337/200'/>
+                    <ImageViewer src='https://picsum.photos/id/437/200'/>
+                    
+                </Stack>
+
+                <Stack sx={{marginTop:'35px', alignItems:'center', cursor:'pointer'}} direction={'row'}>
+                    <Star style={{color:theme==='dark'?'white':'black', marginRight:'10px'}} size={20} />
+                    <Typography sx={{ color:theme==='dark'?'white':'black', fontSize:'17px'}}>Starred messages</Typography>
+                    <CaretRight size={20} style={{marginLeft:'auto', color:theme==='dark'?'white':'black'}} />
+                </Stack>
+
+                <Stack sx={{marginTop:'15px', alignItems:'center'}} direction={'row'}>
+                    <Bell style={{color:theme==='dark'?'white':'black', marginRight:'10px'}} size={20} />
+                    <Typography sx={{ color:theme==='dark'?'white':'black', fontSize:'17px'}}>Mute notifications</Typography>
+                    <StyledSwitch sx={{marginLeft:'auto'}}/>
+                </Stack>
+
+                <Stack  sx={{justifyContent:'space-between', marginTop:'20px'}}>
+                    <Button color="error">Block</Button>
+                    <Button color="error">Clear Chat</Button>
+                </Stack>
+                
+
+            </div>
             
+        )
+    }
+
+    function Attachments(){
+
+        const [value, setValue] = useState(0);
+
+        const handleChange = (event, newValue) => {
+          setValue(newValue);
+        };
+
+        const textTheme = createTheme({
+            palette: {
+                primary:{
+                main: '#4cb5f9', //main color
+                light: '#81d4fa', // Lighter shade of the main color
+                dark: '#2196f3', // Darker shade of the main color
+                contrastText: '#fff', // Text color that contrasts well with the main color
+            },
+                secondary: {
+                    main: '#4cb5f9', // Your main color
+                    light: '#81d4fa', // Lighter shade of the main color
+                    dark: '#2196f3', // Darker shade of the main color
+                    contrastText: '#fff', // Text color that contrasts well with the main color
+            }
+
             
-                <IconButton onClick={()=>dispatch(toggleContactDetail())} 
-                    sx={{alignSelf:'end', color:theme==='dark'?'white':'black'}}>
-                    <X size={20} weight="light" />
+          }});
+
+          
+        return(
+            <div style={{ display:isVisible?"flex":'none',
+                        flexDirection:'column',
+                        width:'360px', 
+                        height:'100svh', 
+                        background: theme==="light"?'rgb(249 249 249)':"black",
+                        transition:'background width 200ms', 
+                        padding:'15px 20px',   
+                        overflow:'auto',
+                        flexShrink:'0',
+                        position:'relative'
+                        }}
+                        className="customScrollbar">
+
+                <IconButton onClick={()=>{setCurrentWindow('basicDetail')}} 
+                        sx={{color:theme==='dark'?'white':'black', position:'absolute', top:'7px', left:'7px'}}>
+                        <CaretLeft size={20} weight="light" />
                 </IconButton>
 
-            
+                <IconButton onClick={()=>{dispatch(toggleContactDetail())
+                                            setCurrentWindow('basicDetail')}} 
+                        sx={{color:theme==='dark'?'white':'black', position:'absolute', top:'7px', right:'7px'}}>
+                        <X size={20} weight="light" />
+                </IconButton>
 
-            <Stack sx={{alignItems:'center',}}>
-                <Avatar sx={{width:'150px', height:'150px'}}
-                        src='https://media.licdn.com/dms/image/D4E03AQHBqWeBZgzLOQ/profile-displayphoto-shrink_800_800/0/1682102966918?e=2147483647&v=beta&t=3h218jhABSX_wWq-yVt8-yCUjO12cSvq-srN_tkYPg8'/>
-            
-                <Typography variant="h6" sx={{ color:theme==='dark'?'white':'black', marginTop:'2px'}}>Pranjal Pravesh</Typography>
-                <Typography variant="h7" sx={{ color:'grey'}}>@pranjal.pravesh</Typography>
-            </Stack>
+                <ThemeProvider theme={textTheme}>
+                    <Tabs sx={{ marginTop: '30px'}} value={value} onChange={handleChange}
+                            variant="fullWidth"
+                            textColor='secondary'
+                            indicatorColor='secondary'>
+                        <Tab label="Media" value={0} sx={{color:theme==='dark'?'white':'black'}}/>
+                        <Tab label="Links" value={1} sx={{color:theme==='dark'?'white':'black'}}/>
+                        <Tab label="Docs" value={2} sx={{color:theme==='dark'?'white':'black'}}/>
+                    </Tabs>
+                </ThemeProvider>
 
+                {value===0?
+                    
+                    <div style={{width:'100%',
+                                marginTop:'30px',
+                                display:'grid',
+                                gridTemplateColumns:'repeat(auto-fit, minmax(min(90px,100%), 1fr))',
+                                justifyItems:'center',
+                                rowGap:'15px'}}>
+                        
 
-            <Stack direction={'row'} sx={{justifyContent:'center', marginTop:'10px', gap:'30px'}}>
-                <IconButton sx={{color:theme==='dark'?'white':'black'}}><Phone size={40} weight="thin" /></IconButton>
-                <IconButton sx={{color:theme==='dark'?'white':'black'}}><VideoCamera size={40} weight="thin" /></IconButton>
-            </Stack>
+                        <ImageViewer style={{width:'95%'}} src='https://strapi.dhiwise.com/uploads/618fa90c201104b94458e1fb_64fecc21e15ea565176c74ad_React_Framer_Motion_Main_Image_07ee7c735e.jpg'/>
+                        <ImageViewer style={{width:'95%'}} src='https://picsum.photos/id/337/200'/>
+                        <ImageViewer style={{width:'95%'}} src='https://picsum.photos/id/437/200'/>
+                        <ImageViewer style={{width:'95%'}} src='https://picsum.photos/id/338/200'/>
+                        <ImageViewer style={{width:'95%'}} src='https://picsum.photos/id/487/200'/>
+                        <ImageViewer style={{width:'95%'}} src='https://picsum.photos/id/737/200'/>
+                        <ImageViewer style={{width:'95%'}} src='https://picsum.photos/id/457/200'/>
 
-
-                <Typography sx={{ color:theme==='dark'?'white':'black', margin:'20px 0 0 0', fontSize:'17px'}}>About</Typography>
-                <Typography sx={{ color:'grey', lineHeight:'18px'}}>
-                    My wave function collapses when you see me.
-                </Typography>
-            
-
-            
-            <Stack sx={{justifyContent:'space-between', marginTop:'30px', alignItems:'center', cursor:'pointer'}} direction={'row'}>
-                <Typography sx={{ color:theme==='dark'?'white':'black', fontSize:'17px'}}>Media, Links & Docs</Typography>
-                <CaretRight size={20} style={{color:theme==='dark'?'white':'black'}}/>
-            </Stack>
-
-            <Stack direction={'row'} sx={{gap:'5px', justifyContent:'center', paddingTop:'15px'}}>
-
-                <ImageViewer src='https://strapi.dhiwise.com/uploads/618fa90c201104b94458e1fb_64fecc21e15ea565176c74ad_React_Framer_Motion_Main_Image_07ee7c735e.jpg'/>
-                <ImageViewer src='https://picsum.photos/id/337/200'/>
-                <ImageViewer src='https://picsum.photos/id/437/200'/>
+                    </div>
                 
-            </Stack>
+                :value===1? //Tab2
 
-            <Stack sx={{marginTop:'35px', alignItems:'center', cursor:'pointer'}} direction={'row'}>
-                <Star style={{color:theme==='dark'?'white':'black', marginRight:'10px'}} size={20} />
-                <Typography sx={{ color:theme==='dark'?'white':'black', fontSize:'17px'}}>Starred messages</Typography>
-                <CaretRight size={20} style={{marginLeft:'auto', color:theme==='dark'?'white':'black'}} />
-            </Stack>
+                <p>Links</p>
 
-            <Stack sx={{marginTop:'15px', alignItems:'center'}} direction={'row'}>
-                <Bell style={{color:theme==='dark'?'white':'black', marginRight:'10px'}} size={20} />
-                <Typography sx={{ color:theme==='dark'?'white':'black', fontSize:'17px'}}>Mute notifications</Typography>
-                <StyledSwitch sx={{marginLeft:'auto'}}/>
-            </Stack>
+                :value===2? //Tab3
+                
+                <p>Documents</p>
 
-            <Stack  sx={{justifyContent:'space-between', marginTop:'20px'}}>
-                <Button color="error">Block</Button>
-                <Button color="error">Clear Chat</Button>
-            </Stack>
-            
+                :null
+                }
+                
 
-        </div>
-        
-    )
+                
+
+            </div>
+        )
+    }
+
+    return currentWindow==='basicDetail'?<BasicDetail />:<Attachments />
 
 }
